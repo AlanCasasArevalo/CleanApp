e1b3f64c64feacf4526509c74184b28607064099
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-08 13:42:41 +0200

    [TEST][REMOTE ADD ACCOUNT TEST] Se agregan el test de fallo en un post con metodos asincronos.

--- DataTests/UsesCases/RemoteAddAccountTest.swift
+++ DataTests/UsesCases/RemoteAddAccountTest.swift
@@ -7,15 +7,31 @@ class RemoteAddAccountTest: XCTestCase {
     func test_add_should_call_httpClient_with_correct_url () {
         let urlToCall = URL(string: "www.google.com")!
         let (sut, httpClientSpy) = makeSut(urlToCall: urlToCall)
-        sut.addAccount(addAccountModel: makeAddAccountModelRequest())
+        // Aqui no se precisa hacer nada en el completionHandler porque no es lo que queremos testar.
+        sut.addAccount(addAccountModel: makeAddAccountModelRequest()) {_ in }
         XCTAssertEqual(httpClientSpy.urlsToCall, [urlToCall])
     }
     
     func test_add_should_call_httpClient_with_correct_data () {
         let (sut, httpClientSpy) = makeSut()
         let addAccountModelRequest = makeAddAccountModelRequest()
-        sut.addAccount(addAccountModel: addAccountModelRequest)
+        // Aqui no se precisa hacer nada en el completionHandler porque no es lo que queremos testar.
+        sut.addAccount(addAccountModel: makeAddAccountModelRequest()) {_ in }
         XCTAssertEqual(httpClientSpy.data, addAccountModelRequest.toData())
+    }
+    
+    func test_add_should_complete_with_error_if_client_fails () {
+        let (sut, httpClientSpy) = makeSut()
+        // Se necesita hacer una nueva variable de expectation debido a que usamos test con respuestas asincronas
+        let expect = expectation(description: "waiting")
+        sut.addAccount(addAccountModel: makeAddAccountModelRequest()) { error in
+            XCTAssertEqual(error, .unexpected)
+            // Aqui hacemos que se libere la espera de la respuesta asincrona
+            expect.fulfill()
+        }
+        httpClientSpy.completeWithError(.noConnectivityError)
+        // Aqui hacemos que espere la respuesta asincrona, de no estar recibiendo los callbacks correspondientes se fallaria el test. 
+        wait(for: [expect], timeout: 5)
     }
 }
 
@@ -33,15 +49,20 @@ extension RemoteAddAccountTest {
     }
     
     class HttpClientSpy: HttpPostClientProtocol {
+
         var urlsToCall = [URL]()
         var data: Data?
-        var callsCount = 0
+        var completationHandler: ((HttpError) -> Void)?
         
-        func post(to urlToCall: URL, with data: Data?) {
+        func post(to urlToCall: URL, with data: Data?, completationHandler: @escaping (HttpError) -> Void) {
             self.urlsToCall.append(urlToCall)
             self.data = data
+            self.completationHandler = completationHandler
         }
         
+        func completeWithError (_ error: HttpError) {
+            completationHandler?(error)
+        }
     }
 }
 
