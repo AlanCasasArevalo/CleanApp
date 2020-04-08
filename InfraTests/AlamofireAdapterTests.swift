adfcad3186629d09d2bdb749ef12030b2dbf3cbb
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-08 18:29:42 +0200

    [TEST][INFRA LAYER TEST] Se crea un test para comprobar que la url a la que tiene que llamar alamofire es la misma a la que estamos llamando se emplea el metodo expectation para hacer los test de los metodos asincronos, Se usa URLProtocols para hacer la captura de los metodos de Alamo

--- InfraTests/AlamofireAdapterTests.swift
+++ InfraTests/AlamofireAdapterTests.swift
@@ -7,10 +7,59 @@
 //
 
 import XCTest
+import Alamofire
 
+class AlamofireAdapter {
+    private let session: Session
+
+    init(session: Session = .default) {
+        self.session = session
+    }
+    
+    func post (to urlToCall: URL) {
+        session.request(urlToCall).resume()
+    }
+}
 class AlamofireAdapterTests: XCTestCase {
 
     func test_ () {
-        let sut = AlamofireAdapter()
+        let urlToCall = makeUrl()
+        let sessionConfiguration = URLSessionConfiguration.default
+        sessionConfiguration.protocolClasses = [UrlProtocolStub.self]
+        let session = Session(configuration: sessionConfiguration)
+        let sut = AlamofireAdapter(session: session)
+        sut.post(to: urlToCall)
+        
+        let expect = expectation(description: "waiting")
+        UrlProtocolStub.observerRequest { request in
+            XCTAssertEqual(urlToCall, request.url)
+            expect.fulfill()
+        }
+        
+        wait(for: [expect], timeout: 2)
+    }
+}
+
+class UrlProtocolStub: URLProtocol {
+    static var emit: ((URLRequest) -> Void)?
+    
+    static func observerRequest (completionHandler: @escaping (URLRequest) -> Void) {
+        UrlProtocolStub.emit = completionHandler
+    }
+    
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    override func startLoading() {
+        UrlProtocolStub.emit?(request)
+    }
+    
+    override func stopLoading() {
+        
     }
 }
