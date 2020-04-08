816300efcad966d7af0874abd85c945e52cbd470
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-08 12:16:11 +0200

    [REFACTOR][ADD ACCOUNT] Se realiza el metodo init publico para que sea accesible desde el data.

--- Domain/AddAccountModelRequest.swift
+++ Domain/AddAccountModelRequest.swift
@@ -6,4 +6,11 @@ public struct AddAccountModelRequest {
     public var email: String
     public var password: String
     public var passwordConfirmation: String
+    
+    public init (name: String, email: String, password: String, passwordConfirmation: String) {
+        self.name = name
+        self.email = email
+        self.password = password
+        self.passwordConfirmation = passwordConfirmation
+    }
 }
