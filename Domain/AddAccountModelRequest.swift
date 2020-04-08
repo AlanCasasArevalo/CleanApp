37c37bbb230a4122791c9d263fff18a72ca12fbc
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-08 12:24:48 +0200

    [REFACTOR][REMOTE ADD ACCOUNT TEST] Se agrega el protocolo Encodable a nuestro modelo de request.

--- Domain/AddAccountModelRequest.swift
+++ Domain/AddAccountModelRequest.swift
@@ -1,7 +1,7 @@
 
 import Foundation
 
-public struct AddAccountModelRequest {
+public struct AddAccountModelRequest: Encodable {
     public var name: String
     public var email: String
     public var password: String
