ea7c685ffd08f4b25fc6a68dd8ac617b2dcd31e4
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-08 11:32:01 +0200

    [TEST][REMOTE ADD ACCOUNT TEST] Se hace lo minimo para pasar el test en este caso crear un inicializador para pasar la url que tiene que ser llamada.

--- DataTests/RemoteAddAccountTest.swift
+++ DataTests/RemoteAddAccountTest.swift
@@ -2,13 +2,18 @@
 import XCTest
 
 class RemoteAddAccount {
+    private let urlToCall: URL
     
+    init(urlToCall: URL) {
+        self.urlToCall = urlToCall
+    }
 }
 
 class RemoteAddAccountTest: XCTestCase {
 
     func test_() {
-        let sut = RemoteAddAccount(urlToCall: url)
+        let urlToCall = URL(string: "www.any_url.com")!
+        let sut = RemoteAddAccount(urlToCall: urlToCall)
     }
 }
 
