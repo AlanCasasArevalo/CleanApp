392c5499bc21c0f6caf3cc45485c71ce99deaa42
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-08 13:59:57 +0200

    [REFACTOR][HTTP CLIENT] Se refactoriza el codigo para que ya no devuelva solo el error si no tambien el dato.

--- Data/Http/HttpPostClientProtocol.swift
+++ Data/Http/HttpPostClientProtocol.swift
@@ -9,5 +9,5 @@
 import Foundation
 
 public protocol HttpPostClientProtocol {
-    func post(to urlToCall: URL, with data: Data?, completationHandler: @escaping (HttpError) -> Void)
+    func post(to urlToCall: URL, with data: Data?, completationHandler: @escaping (Result<Data, HttpError>) -> Void)
 }
