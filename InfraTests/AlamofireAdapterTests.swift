01b57453a53be9c64702d40c7381ef88fcdf2a02
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-08 17:51:57 +0200

    [FEATURE][INFRA LAYER] Se crea un nuevo target para hacer los test de la integracion con el domain y data

---
+++ InfraTests/AlamofireAdapterTests.swift
@@ -0,0 +1,16 @@
+//
+//  InfraTests.swift
+//  InfraTests
+//
+//  Created by Alan Casas on 08/04/2020.
+//  Copyright © 2020 Alan Casas. All rights reserved.
+//
+
+import XCTest
+
+class AlamofireAdapterTests: XCTestCase {
+
+    func test_ () {
+        
+    }
+}
