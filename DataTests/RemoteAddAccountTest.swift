a2016d4a83743b73c0a511587804b0df04ed6a87
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-08 11:41:22 +0200

    [TEST][REMOTE ADD ACCOUNT TEST] Se crean el protocolo HttpClientProtocol y una clase espia (mock) para poder implementar todo lo necesario en dicha clase y siempre estar haciendo esta clase FAKE para no depender nunca de test contra internet

--- DataTests/RemoteAddAccountTest.swift
+++ DataTests/RemoteAddAccountTest.swift
@@ -9,12 +9,26 @@ class RemoteAddAccount {
     }
 }
 
+protocol HttpClientProtocol {
+    func post(urlToCall: URL)
+}
+
 class RemoteAddAccountTest: XCTestCase {
+    
+    class HttpClientSpy: HttpClientProtocol {
+        var urlToCall: URL?
+
+        func post(urlToCall: URL) {
+            
+        }
+        
+    }
 
     func test_() {
         let urlToCall = URL(string: "www.any_url.com")!
+        let httpClient = HttpClientSpy()
         let sut = RemoteAddAccount(urlToCall: urlToCall)
-        XCTAssertEqual(httpClient.url, urlToCall)
+        XCTAssertEqual(httpClient.urlToCall, urlToCall)
     }
 }
 
