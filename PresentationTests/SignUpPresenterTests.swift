9b36940d3b05e6fcc8a73da43e3026464a2c73da
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-12 11:22:42 +0200

    [CONFITURATION][TARGET PRESENTATION] Creamos un nuevo target que sera el target de presentacion

---
+++ PresentationTests/SignUpPresenterTests.swift
@@ -0,0 +1,17 @@
+//
+//  PresentationTests.swift
+//  PresentationTests
+//
+//  Created by Alan Casas on 12/04/2020.
+//  Copyright © 2020 Alan Casas. All rights reserved.
+//
+
+import XCTest
+
+class SignUpPresenterTests: XCTestCase {
+
+    func test_ () {
+        
+    }
+
+}
