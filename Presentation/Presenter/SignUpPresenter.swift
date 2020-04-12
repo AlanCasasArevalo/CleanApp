6f85dce989dcb26c580d5a7df7422eb3ccc1774a
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-12 17:46:25 +0200

    [FEATURE][PRESENTATION] Se repara la fuga de memoria

--- Presentation/Presenter/SignUpPresenter.swift
+++ Presentation/Presenter/SignUpPresenter.swift
@@ -26,7 +26,8 @@ public final class SignUpPresenter {
             alertView.showMessage(viewModel: AlertViewModel(title: "Falla la validacion", message: errorMessage))
         } else {
             let addAccountModel = AddAccountModelRequest(name: viewModel.name!, email: viewModel.email!, password: viewModel.password!, passwordConfirmation: viewModel.passwordConfirmation!)
-            addAccount.addAccount(addAccountModel: addAccountModel) { (result) in
+            addAccount.addAccount(addAccountModel: addAccountModel) { [weak self] (result) in
+                guard let self = self else { return }
                 switch result {
                 case .success: break
                 case .failure:
