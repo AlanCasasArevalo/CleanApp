883b81ff422fefc77f9d9956c5b9061532198787
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-13 21:13:25 +0200

    [FEATURE][EMAIL VALIDATOR ADAPTER] Se crea la clase que hara la validacion del email

--- ValidationTests/EmailValidatorAdapterTests.swift
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -10,6 +10,14 @@ import XCTest
 import Presentation
 
 
+class EmailValidatorAdapter: EmailValidatorProtocol {
+    private let patter = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+    
+    func isEmailValid(email: String) -> Bool {
+        return true
+    }
+}
+
 class EmailValidatorAdapterTests: XCTestCase {
 
     func test_invalid_emails () {
