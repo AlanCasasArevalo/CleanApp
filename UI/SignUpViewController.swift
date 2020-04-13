93e70ff17627d9d88e5eeb08d782aed2da74e119
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-13 11:22:00 +0200

    [FEATURE][SignUpViewController] Se crea la clase con StoryBoard de Signup con un activity indicator

---
+++ UI/SignUpViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpViewController.swift
+//  UI
+//
+//  Created by Alan Casas on 13/04/2020.
+//  Copyright © 2020 Alan Casas. All rights reserved.
+//
+
+import UIKit
+
+final class SignUpViewController: UIViewController {
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
