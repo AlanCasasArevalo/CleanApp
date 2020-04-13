af75cd2a6cc5d8498c6304edf3d061257cced5a4
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-13 11:36:49 +0200

    [FEATURE][SignUpViewController] Se crea una extension para implementar el protocolo de Loader view

--- UI/SignUpViewController.swift
+++ UI/SignUpViewController.swift
@@ -7,11 +7,22 @@
 //
 
 import UIKit
+import Presentation
 
 final class SignUpViewController: UIViewController {
     @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+extension SignUpViewController: LoaderViewProtocol {
+    func showLoader(viewModel: LoaderViewModel) {
+        if viewModel.isLoading {
+            loadingIndicator.startAnimating()
+        } else {
+            loadingIndicator.stopAnimating()
+        }
     }
 }
