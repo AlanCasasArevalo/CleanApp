defda490f50d9d32411f3e7dddba23c033297e4b
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-13 11:35:52 +0200

    [TEST][UI TEST] Se crea el test de comprobacion de que el viewcontroller no sea nulo Se crean los test de implementacion de protocolo de loader view

--- UITests/SigUpViewControllerTest.swift
+++ UITests/SigUpViewControllerTest.swift
@@ -8,10 +8,21 @@
 
 import XCTest
 import UIKit
+import Presentation
 @testable import UI
 
 class SigUpViewControllerTest: XCTestCase {
-
+    
+    func test_sign_up_is_not_nil () {
+        // Si usas StoryBoards necesitas inicializar el Sb
+        let sb = UIStoryboard(name: "SignUpViewController", bundle: Bundle(for: SignUpViewController.self))
+        let sut = sb.instantiateViewController(identifier: "SignUpViewController") as! SignUpViewController
+        sut.beginAppearanceTransition(true, animated: false)
+        sut.loadViewIfNeeded()
+        XCTAssertNotNil(sut)
+        sut.endAppearanceTransition()
+    }
+    
     func test_loading_is_hidden_on_start () {
         // Si usas StoryBoards necesitas inicializar el Sb
         let sb = UIStoryboard(name: "SignUpViewController", bundle: Bundle(for: SignUpViewController.self))
@@ -22,5 +33,13 @@ class SigUpViewControllerTest: XCTestCase {
         XCTAssertEqual(sut.loadingIndicator?.isAnimating, false)
         sut.endAppearanceTransition()
     }
-
+    
+    func test_sut_is_implements_loaderViewProtocol () {
+        let sb = UIStoryboard(name: "SignUpViewController", bundle: Bundle(for: SignUpViewController.self))
+        let sut = sb.instantiateViewController(identifier: "SignUpViewController") as! SignUpViewController
+        sut.beginAppearanceTransition(true, animated: false)
+        sut.loadViewIfNeeded()
+        XCTAssertNotNil(sut as LoaderViewProtocol)
+        sut.endAppearanceTransition()
+    }
 }
