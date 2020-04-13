97ca93a2c60b848ebd2a93457adde1434d4fe4b4
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-13 09:34:31 +0200

    [CONFITURATION][TARGET UI] Se crea el target de UI

---
+++ UITests/SigUpViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UITests.swift
+//  UITests
+//
+//  Created by Alan Casas on 13/04/2020.
+//  Copyright © 2020 Alan Casas. All rights reserved.
+//
+
+import XCTest
+
+class SigUpViewController: XCTestCase {
+
+    func test_() {
+        
+    }
+
+}
