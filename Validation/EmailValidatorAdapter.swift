7f673e72ec4cffbda227b7d91dbfeaf7ec2298c1
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-13 21:30:41 +0200

    [REFACTOR][EMAIL VALIDATOR ADAPTER TEST] Se elimina la clase de EmailValidatorAdapter de produccion de nuestra clase de test moviendo dicha clase a su propio fichero.

---
+++ Validation/EmailValidatorAdapter.swift
@@ -0,0 +1,24 @@
+//
+//  EmailValidatorAdapter.swift
+//  Validation
+//
+//  Created by Alan Casas on 13/04/2020.
+//  Copyright © 2020 Alan Casas. All rights reserved.
+//
+
+import Foundation
+import Presentation
+
+public class EmailValidatorAdapter: EmailValidatorProtocol {
+    
+    private let patter = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+    
+    public init () {}
+    
+    public func isEmailValid(email: String) -> Bool {
+        let range = NSRange(location: 0, length: email.utf16.count)
+        let regex = try? NSRegularExpression(pattern: patter)
+        let match = regex?.firstMatch(in: email, options: [], range: range) != nil
+        return match
+    }
+}
--- ValidationTests/EmailValidatorAdapterTests.swift
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -8,18 +8,7 @@
 
 import XCTest
 import Presentation
-
-
-public class EmailValidatorAdapter: EmailValidatorProtocol {
-    private let patter = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-    
-    public func isEmailValid(email: String) -> Bool {
-        let range = NSRange(location: 0, length: email.utf16.count)
-        let regex = try? NSRegularExpression(pattern: patter)
-        let match = regex?.firstMatch(in: email, options: [], range: range) != nil
-        return match
-    }
-}
+import Validation
 
 class EmailValidatorAdapterTests: XCTestCase {
 
