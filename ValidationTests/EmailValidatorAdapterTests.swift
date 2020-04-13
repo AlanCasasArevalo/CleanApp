8156a179df8f74f7a4abda7dd932166512be4dfe
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-13 21:02:28 +0200

    [CONFITURATION][VALIDATOR TARGET]

---
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -0,0 +1,17 @@
+//
+//  EmailValidatorAdapterTests.swift
+//  ValidationTests
+//
+//  Created by Alan Casas on 13/04/2020.
+//  Copyright © 2020 Alan Casas. All rights reserved.
+//
+
+import XCTest
+import Presentation
+
+class EmailValidatorAdapterTests: XCTestCase {
+
+    func test_ () {
+    }
+
+}
