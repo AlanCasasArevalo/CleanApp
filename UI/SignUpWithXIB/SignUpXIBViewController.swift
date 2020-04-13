29018b08ad21a03666331bc262f33d3c2f7af26a
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-13 17:44:51 +0200

    [FEATURE][SignUpViewControllerXIB] Se crean los archivos correspondientes de XIB para poder tener ejemplos tanto con storyboards como con XIB

---
+++ UI/SignUpWithXIB/SignUpXIBViewController.swift
@@ -0,0 +1,56 @@
+//
+//  SignUpXIBViewController.swift
+//  UI
+//
+//  Created by Alan Casas on 13/04/2020.
+//  Copyright © 2020 Alan Casas. All rights reserved.
+//
+
+import UIKit
+import Presentation
+
+final class SignUpXIBViewController: UIViewController {
+
+    @IBOutlet weak var loaderView: UIActivityIndicatorView!
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var nameTextField : UITextField!
+    @IBOutlet weak var emailTextField : UITextField!
+    @IBOutlet weak var passwordTextField : UITextField!
+    @IBOutlet weak var passwordConfirmationTextField : UITextField!
+    
+    var signUp: ((SignUpViewModel) -> Void)?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+    }
+
+}
+
+extension SignUpXIBViewController {
+    private func configure(){
+        saveButton.addTarget(self, action: #selector(saveButtonTouched), for: .touchUpInside)
+    }
+    
+    @objc private func saveButtonTouched() {
+        let viewModel: SignUpViewModel = SignUpViewModel(name: nameTextField.text, email: emailTextField.text, password: passwordTextField.text, passwordConfirmation: passwordConfirmationTextField.text)
+        signUp?(viewModel)
+    }
+    
+}
+
+extension SignUpXIBViewController: LoaderViewProtocol {
+    func showLoader(viewModel: LoaderViewModel) {
+        if viewModel.isLoading {
+            loaderView.startAnimating()
+        } else {
+            loaderView.stopAnimating()
+        }
+    }
+}
+
+extension SignUpXIBViewController: AlertViewProtocol {
+    func showMessage(viewModel: AlertViewModel) {
+        
+    }
+}
