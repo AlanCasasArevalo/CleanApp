407b8ad6f00a020d92a91e62a980170d3fce211c
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-13 12:36:16 +0200

    [REFACTOR][PRESENTATION] Se modifica la clase de modelo SignUpViewModel para que implemente el protocolo de modelo

--- Presentation/ViewModel/SignUpViewModel.swift
+++ Presentation/ViewModel/SignUpViewModel.swift
@@ -7,8 +7,9 @@
 //
 
 import Foundation
+import Domain
 
-public struct SignUpViewModel {
+public struct SignUpViewModel: ModelProtocol {
     public var name: String?
     public var email: String?
     public var password: String?
