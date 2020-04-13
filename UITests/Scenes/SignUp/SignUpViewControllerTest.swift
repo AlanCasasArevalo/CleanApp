bb3893d5903a54b19f11fc7e7dbb56d7b1bae115
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-13 17:50:55 +0200

    [REFACTOR][UI TEST] Se separa las clases a sus propios archivos y carpetas

--- UITests/Scenes/SignUp/SignUpViewControllerTest.swift
+++ UITests/Scenes/SignUp/SignUpViewControllerTest.swift
@@ -76,17 +76,3 @@ extension SignUpViewControllerTest {
         return sut
     }
 }
-
-extension UIControl {
-    func simulate (event: UIControl.Event) {
-        allTargets.forEach { target in
-            self.actions(forTarget: target, forControlEvent: event)?.forEach({ action in
-                (target as? NSObject)?.perform(Selector(action))
-            })
-        }
-    }
-    
-    func simulateTap() {
-        simulate(event: .touchUpInside)
-    }
-}
