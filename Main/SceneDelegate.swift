3277403a050631fa166738b72011065167e8681f
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-14 09:43:38 +0200

    [CONFITURATION][MAIN TARGET] Se agrega el target encargado de arrancar la aplicacion

---
+++ Main/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  Main
+//
+//  Created by Alan Casas on 14/04/2020.
+//  Copyright © 2020 Alan Casas. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
