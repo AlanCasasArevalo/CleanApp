49ae1212e93470ac3e8953f48e7acc119835caba
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-14 17:08:26 +0200

    [TEST][BASE URL VARIABLE TEST] Se crea un test para comprobar que las url de produccion y de debug son distintas

--- MainTests/SignUpIntegrationTests.swift
+++ MainTests/SignUpIntegrationTests.swift
@@ -15,5 +15,20 @@ class SignUpIntegrationTests: XCTestCase {
         let sut = SignUpComposer.composeControllerWith(addAccount: AddAccountSpy())
         checkMemoryLeak(for: sut)
     }
+    
+    func test_enviroment_correct_api_base_url () {
+        let valueToTest = getValue(for: EnviromentHelper.EnviromentVariable.apiBaseUrl.rawValue)
+        #if DEBUG
+        XCTAssertNotNil(valueToTest)
+        XCTAssertEqual(valueToTest, "http://localhost:5050/api")
+        #elseif RELEASE
+        XCTAssertNotNil(valueToTest)
+        XCTAssertEqual(valueToTest, "http://localhost:5050/api")
+        #endif
+    }
+    
+    func getValue(for key: String) -> String? {
+        return Bundle.main.infoDictionary?[key] as? String
+    }
 
 }
