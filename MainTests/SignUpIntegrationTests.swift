86f915e19c7c8ac871c48cf96f227905ab6b31bb
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-14 11:43:01 +0200

    [TEST][SIGN UP] Se crea un test para comprobar la integracion de la arquitectura y comprobar si hay memory leaks

---
+++ MainTests/SignUpIntegrationTests.swift
@@ -0,0 +1,19 @@
+//
+//  SignUpIntegrationTests.swift
+//  MainTests
+//
+//  Created by Alan Casas on 14/04/2020.
+//  Copyright © 2020 Alan Casas. All rights reserved.
+//
+
+import XCTest
+import Main
+
+class SignUpIntegrationTests: XCTestCase {
+
+    func test_() {
+        let sut = SignUpComposer.composeControllerWith(addAccount: AddAccountSpy())
+        checkMemoryLeak(for: sut)
+    }
+
+}
