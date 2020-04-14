d484705ab3e8804b19f1efd54d3294d5509682a9
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-14 16:16:19 +0200

    [REFACTOR][SIGN UP ARCHITECTURE] Se cambia un poco la arquitectura para que sea mas legible

--- Main/Composers/SignUpComposer.swift
+++ Main/Composers/SignUpComposer.swift
@@ -9,10 +9,25 @@
 import Foundation
 import Domain
 import UI
+import Validation
+import Presentation
+
 
 public final class SignUpComposer {
     public static func composeControllerWith (addAccount: AddAccountProtocol) -> SignUpViewController {
-        let signUp = ControllerFactory.makeSignUp(addAccount: addAccount)
-        return signUp
+        let viewController = SignUpViewController.instantiate()
+        viewController.signUp = presenter(viewController: viewController, addAccount: addAccount).signUp
+        return viewController
     }
+    
+    static func presenter (viewController: SignUpViewController, addAccount: AddAccountProtocol) -> SignUpPresenter {
+        let presenter = SignUpPresenter(alertView: WeakVarProxy(viewController), emailValidator: validator(), addAccount: addAccount, loaderView: WeakVarProxy(viewController))
+        return presenter
+    }
+    
+    static func validator () -> EmailValidatorAdapter {
+        let validator = EmailValidatorAdapter()
+        return validator
+    }
+    
 }
