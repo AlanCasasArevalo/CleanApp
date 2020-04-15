39eebb74cc730a4e321f2883f84181185ac03938
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-15 09:40:33 +0200

    [TEST][SIGN UP ARCHITECTURE] Se crea el test que hace que si la UI es manipulada en segundo plano rompa la app

--- MainTests/SignUpComposerTests.swift
+++ MainTests/SignUpComposerTests.swift
@@ -9,6 +9,7 @@
 import XCTest
 import Main
 import UI
+import Presentation
 
 class SignUpComposerTests: XCTestCase {
     
@@ -32,16 +33,21 @@ class SignUpComposerTests: XCTestCase {
     func test_background_request_should_complete_on_main_thread () {
         let (sut, addAccountSpy) = makeSut()
         sut.loadViewIfNeeded()
+        sut.signUp?(makeSignUpViewModel())
+        let expect = expectation(description: "waiting")
+        DispatchQueue.global().async {
+            addAccountSpy.completionWithError(error: .unexpected)
+            expect.fulfill()
+        }
+        wait(for: [expect], timeout: 2)
     }
-    
-    
-    
 }
 
 extension SignUpComposerTests {
     func makeSut (file: StaticString = #file, line: UInt = #line) -> (sut: SignUpViewController, addAccountSpy: AddAccountSpy){
         let addAccountSpy = AddAccountSpy()
-        let sut = SignUpComposer.composeControllerWith(addAccount: addAccountSpy)
+        let remoteAddAccount = MainQueueDispatchDecorator(addAccountSpy)
+        let sut = SignUpComposer.composeControllerWith(addAccount: remoteAddAccount)
         checkMemoryLeak(for: sut, file: file, line: line)
         checkMemoryLeak(for: addAccountSpy, file: file, line: line)
         return (sut, addAccountSpy)
