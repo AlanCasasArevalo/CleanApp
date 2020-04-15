08f402c8b610a6a04471e4300c3bba28a6379e82
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-15 09:41:29 +0200

    [FEATURE][SIGN UP ARCHITECTURE] Se crean metodos para poder hacer que la UI no sea manipulada en segundo plano.

--- Main/Factory/UseCaseFactory.swift
+++ Main/Factory/UseCaseFactory.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import Domain
 import Data
 import Infra
 
@@ -14,9 +15,9 @@ public class UseCaseFactory {
     
     private static let apiBasicUrl = EnviromentHelper.variable(keyToAcces: .apiBaseUrl)
     
-    static func remoteAddAccount () -> RemoteAddAccount {
+    static func remoteAddAccount () -> AddAccountProtocol {
         let remoteAddAccount = RemoteAddAccount(urlToCall: makeUrlWith(path: "signup"), httpClient: httpClient())
-        return remoteAddAccount
+        return MainQueueDispatchDecorator(remoteAddAccount)
     }
     
     static func httpClient () -> AlamofireAdapter {
@@ -27,5 +28,30 @@ public class UseCaseFactory {
     static func makeUrlWith (path: String) -> URL {
         let url = URL(string: "\(apiBasicUrl)/\(path)")!
         return url
+    }
+}
+
+public final class MainQueueDispatchDecorator<T>  {
+    private let instance: T
+    
+    public init(_ instance: T) {
+        self.instance = instance
+    }
+    
+    func dispatch (completionHandler: @escaping () -> Void) {
+        guard Thread.isMainThread else {
+            return DispatchQueue.main.async(execute: completionHandler)
+        }
+        completionHandler()
+    }
+}
+ 
+extension MainQueueDispatchDecorator: AddAccountProtocol where T: AddAccountProtocol {
+    public func addAccount(addAccountModel: AddAccountModelRequest, completionHandler: @escaping (Result<AccountModel, DomainError>) -> Void) {
+        instance.addAccount(addAccountModel: addAccountModel) { [weak self] result in
+            self?.dispatch {
+                completionHandler(result)
+            }
+        }
     }
 }
