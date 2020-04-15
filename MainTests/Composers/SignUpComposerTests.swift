f428444de90cc25c0b7fbc392715e574c17a2ded
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-15 08:18:54 +0200

    [REFACTOR][SIGN UP COMPOSER TEST] Se realizan todos los cambios correspondientes para que se realice la inicializacion en un metodo que sera el encargado de hacer las inyecciones de dependencia.

--- MainTests/Composers/SignUpComposerTests.swift
+++ MainTests/Composers/SignUpComposerTests.swift
@@ -8,11 +8,12 @@
 
 import XCTest
 import Main
+import UI
 
 class SignUpComposerTests: XCTestCase {
-
+    
     func test_memory_leak_integration () {
-        let sut = SignUpComposer.composeControllerWith(addAccount: AddAccountSpy())
+        let (sut, _) = makeSut()
         checkMemoryLeak(for: sut)
     }
     
@@ -27,10 +28,27 @@ class SignUpComposerTests: XCTestCase {
         XCTAssertNotNil(valueToTest)
         XCTAssertEqual(valueToTest, "https://clean-node-api.herokuapp.com/api")
     }
-
+    
+    func test_background_request_should_complete_on_main_thread () {
+        let (sut, addAccountSpy) = makeSut()
+        sut.loadViewIfNeeded()
+    }
+    
+    
+    
 }
 
-extension SignUpComposerTests {    
+extension SignUpComposerTests {
+    func makeSut (file: StaticString = #file, line: UInt = #line) -> (sut: SignUpViewController, addAccountSpy: AddAccountSpy){
+        let addAccountSpy = AddAccountSpy()
+        let sut = SignUpComposer.composeControllerWith(addAccount: addAccountSpy)
+        checkMemoryLeak(for: sut, file: file, line: line)
+        checkMemoryLeak(for: addAccountSpy, file: file, line: line)
+        return (sut, addAccountSpy)
+    }
+}
+
+extension SignUpComposerTests {
     func getValue(for key: String) -> String? {
         return Bundle.main.infoDictionary?[key] as? String
     }
